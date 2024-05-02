cc641491fcbcc749b31c54a4253c52f656d4c042
dave8git <dave8git@gmail.com>
2024-05-02 23:38:23 +0100

    ADD generating list on the leftside column

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const titleClickHandler = function(event) {
+const titleClickHandler = function (event) {
     event.preventDefault();
     console.log('Link was clicked!');
     const allActiveLinks = document.querySelectorAll('.titles a.active');
@@ -13,8 +13,39 @@ const titleClickHandler = function(event) {
     document.querySelector(this.getAttribute('href')).classList.add('active');
 }
 
-const allLinks = document.querySelectorAll('.titles a');
 
-for(let link of allLinks) {
-    link.addEventListener('click', titleClickHandler);
+
+const optLinksSelector = '.titles';
+const optPostSelector = '.post';
+const optArticleTitleSelector = '.post-title';
+
+const generateTitleLinks = function (event) {
+    document.querySelector(optLinksSelector).innerHTML = '';
+
+    const allArticles = document.querySelectorAll(optPostSelector);
+
+    let articleLinks = ' ';
+
+    allArticles.forEach((article) => {
+        const articleId = article.getAttribute('id');
+        const articleTitle = article.querySelector('.post-title').innerHTML;
+
+        console.log(articleId);
+        console.log(articleTitle);
+
+        const htmlLink = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>'
+
+        articleLinks += htmlLink;
+    });
+
+    console.log(articleLinks);
+    document.querySelector(optLinksSelector).insertAdjacentHTML("beforeend", articleLinks);
+
+    const allLinks = document.querySelectorAll('.titles a');
+
+    for (let link of allLinks) {
+        link.addEventListener('click', titleClickHandler);
+    }
 }
+
+generateTitleLinks(); 
